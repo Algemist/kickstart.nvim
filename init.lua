12722047ffdfec12b4f1950fec58621ab7f48b64
Давыдов Денис <d.a.davydov@foxford.ru>
2024-06-04 23:17:20 +0800

    change colorscheme

--- init.lua
+++ init.lua
@@ -535,11 +535,15 @@ require('lazy').setup({
         italic = false,
       },
     },
+  },
+  {
+    'olimorris/onedarkpro.nvim',
+    priority = 1000, -- Ensure it loads first
     init = function()
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
+      vim.cmd.colorscheme 'onedark_dark'
 
       -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=none'
